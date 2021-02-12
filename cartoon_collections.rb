701c92ee89ad8c1527f7b9f1b10c7d7f81ac342b
Nicholas Frangione <nfrangione8@gmail.com>
2021-02-12 20:21:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |character|
-    puts "#{character.capitalize}"
+    puts "Hello #{character.capitalize}!"
 end
 
 def list_dwarves(array)
